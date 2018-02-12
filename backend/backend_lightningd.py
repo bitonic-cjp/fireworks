e8c23c8c204a16b5af6cf3507cd5184d4adb86df
Corné Plooy <corne@bitonic.nl>
2018-02-12 17:26:10 +0100

    Start using the lightningd RPC; allow the configuration to be adapted through the commandline

--- backend/backend_lightningd.py
+++ backend/backend_lightningd.py
@@ -1,10 +1,20 @@
+import os.path
 import logging
+
+from backend.lightningd.lightning import LightningRpc
 
 
 
 class Backend:
     def __init__(self, config):
         logging.info('Using Lightningd back-end')
+        lightningDir = config.getValue('lightningd', 'lightning-dir')
+        lightningDir = os.path.expanduser(lightningDir)
+        lightningDir = os.path.abspath(lightningDir)
+        socketFile = os.path.join(lightningDir, 'lightning-rpc')
+        self.rpc = LightningRpc(socketFile)
+        print(self.rpc.dev_blockheight())
+
 
 
 logging.info('Loaded Lightningd back-end module')
--- configuration.py
+++ configuration.py
@@ -1,17 +1,42 @@
+import sys
+
+
+
 class Configuration:
     def __init__(self):
         #For now, configuration is hard-coded.
-        #In the future, load from commandline args and config file.
+        #In the future, load from config file.
         self.sections = \
         {
         'modules':
             {
             'frontend': 'qt',
             'backend': 'lightningd'
+            },
+        'lightningd':
+            {
+            'lightning-dir': '~/.lightning'
             }
         }
+        self.readCommandline()
+
+
+    def readCommandline(self):
+        for arg in sys.argv[1:]:
+            try:
+                name, value = arg.split('=')
+                section, name = name.split('/')
+                self.setValue(section, name, value)
+            except ValueError:
+                continue #different syntax - ignore
 
 
     def getValue(self, section, name):
         return self.sections[section][name]
+
+
+    def setValue(self, section, name, value):
+        if section not in self.sections:
+            self.sections[section] = {}
+        self.sections[section][name] = value
 
