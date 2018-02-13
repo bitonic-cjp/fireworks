388b12fcd1f2b8d3d55b37826f7685a2f716c8ca
Corné Plooy <corne@bitonic.nl>
2018-02-13 17:02:04 +0100

    Typo fix.

--- frontend/qt/mainwindow.py
+++ frontend/qt/mainwindow.py
@@ -74,21 +74,21 @@ class MainWindow(QMainWindow):
     def showAbout(self):
         QMessageBox.about(self, 'Fireworks', \
 '''
-Firworks
+Fireworks
 Copyright (C) 2018 Bitonic B.V.
 
-Firworks is free software: you can redistribute it and/or modify
+Fireworks is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Firworks is distributed in the hope that it will be useful,
+Fireworks is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Firworks. If not, see <http://www.gnu.org/licenses/>.
+along with Fireworks. If not, see <http://www.gnu.org/licenses/>.
 
 The source code is available at https://github.com/bitonic-cjp/fireworks
 ''')
