84b3a096a33c9fa83bfac8816e340527587d5440
Corné Plooy <corne@bitonic.nl>
2018-02-13 13:56:46 +0100

    Implement command history

--- frontend/qt/console.py
+++ frontend/qt/console.py
@@ -19,6 +19,9 @@ class Console(QPlainTextEdit):
         self.setUndoRedoEnabled(False)
         self.document().setDefaultFont(QtGui.QFont(MONOSPACE_FONT, 10, QtGui.QFont.Normal))
 
+        self.history = []
+        self.history_index = 0
+
         self.newPrompt()
 
 
@@ -35,14 +38,54 @@ class Console(QPlainTextEdit):
         return curr_line
 
 
+    def setCommand(self, command):
+        if self.getCommand() == command:
+            return
+
+        doc = self.document()
+        curr_line = doc.findBlockByLineNumber(doc.lineCount() - 1).text()
+        self.moveCursor(QtGui.QTextCursor.End)
+        for i in range(len(curr_line) - len(self.prompt)):
+            self.moveCursor(QtGui.QTextCursor.Left, QtGui.QTextCursor.KeepAnchor)
+
+        self.textCursor().removeSelectedText()
+        self.textCursor().insertText(command)
+        self.moveCursor(QtGui.QTextCursor.End)
+
+
     def getCursorPosition(self):
         c = self.textCursor()
         return c.position() - c.block().position() - len(self.prompt)
+
 
     def setCursorPosition(self, position):
         self.moveCursor(QtGui.QTextCursor.StartOfLine)
         for i in range(len(self.prompt) + position):
             self.moveCursor(QtGui.QTextCursor.Right)
+
+
+    def addToHistory(self, command):
+        if command[0:1] == ' ':
+            return
+
+        if command and (not self.history or self.history[-1] != command):
+            self.history.append(command)
+        self.history_index = len(self.history)
+
+
+    def getPrevHistoryEntry(self):
+        if self.history:
+            self.history_index = max(0, self.history_index - 1)
+            return self.history[self.history_index]
+        return ''
+
+    def getNextHistoryEntry(self):
+        if self.history:
+            hist_len = len(self.history)
+            self.history_index = min(hist_len, self.history_index + 1)
+            if self.history_index < hist_len:
+                return self.history[self.history_index]
+        return ''
 
 
     def keyPressEvent(self, event):
@@ -57,12 +100,20 @@ class Console(QPlainTextEdit):
         elif event.key() in (QtCore.Qt.Key_Left, QtCore.Qt.Key_Backspace):
             if self.getCursorPosition() == 0:
                 return
+        elif event.key() == QtCore.Qt.Key_Up:
+            self.setCommand(self.getPrevHistoryEntry())
+            return
+        elif event.key() == QtCore.Qt.Key_Down:
+            self.setCommand(self.getNextHistoryEntry())
+            return
 
         super(Console, self).keyPressEvent(event)
 
 
     def runCommand(self):
         command = self.getCommand()
+        self.addToHistory(command)
+
         self.appendPlainText('Command output\n')
         self.newPrompt()
 
