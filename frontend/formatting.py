cba9798a578ad301d7e8f9c65d3405b6a7f5e174
Corné Plooy <corne@bitonic.nl>
2018-03-05 16:06:02 +0100

    Support (un)formatting of different currencies and units.

--- frontend/formatting.py
+++ frontend/formatting.py
@@ -18,6 +18,8 @@
 import datetime
 import decimal
 
+from utils.struct import Struct
+
 
 
 def formatTimestamp(timestamp):
@@ -42,40 +44,110 @@ def formatTimestamp(timestamp):
 
 
 
-amountUnits = \
+class CurrencyInfo(Struct):
+    defaultUnit = None #str
+    multipliers = {}   #dict of str->int: multiplier in mSatoshi
+
+
+currencyInfo = \
 {
-'mSatoshi':            1,
-'Satoshi' :         1000,
-'uBTC'    :       100000,
-'mBTC'    :    100000000,
-'BTC'     : 100000000000,
+'bc': CurrencyInfo( #Bitcoin
+    defaultUnit='BTC',
+    multipliers={
+    'mSatoshi':            1,
+    'Satoshi' :         1000,
+    'uBTC'    :       100000,
+    'mBTC'    :    100000000,
+    'BTC'     : 100000000000,
+    }),
+'tb': CurrencyInfo( #Bitcoin-testnet
+    defaultUnit='tBTC',
+    multipliers={
+    'mSatoshi':            1,
+    'Satoshi' :         1000,
+    'utBTC'   :       100000,
+    'mtBTC'   :    100000000,
+    'tBTC'    : 100000000000,
+    }),
+'bcrt': CurrencyInfo( #Bitcoin-regtest
+    defaultUnit='rBTC',
+    multipliers={
+    'mSatoshi':            1,
+    'Satoshi' :         1000,
+    'urBTC'   :       100000,
+    'mrBTC'   :    100000000,
+    'rBTC'    : 100000000000,
+    }),
+
+
+'ltc': CurrencyInfo( #Litecoin
+    defaultUnit='LTC',
+    multipliers={
+    'mlitoshi':            1,
+    'litoshi' :         1000,
+    'uLTC'    :       100000,
+    'mLTC'    :    100000000,
+    'LTC'     : 100000000000,
+    }),
+'ltct': CurrencyInfo( #Litecoin-testnet
+    defaultUnit='tLTC',
+    multipliers={
+    'mlitoshi':            1,
+    'litoshi' :         1000,
+    'utLTC'   :       100000,
+    'mtLTC'   :    100000000,
+    'tLTC'    : 100000000000,
+    }),
 }
 
 
-def formatAmount(amount, currency='bc'): #TODO: assume no default!
+def formatAmount(amount, currency='bc', unit=None): #TODO: assume no default!
     '''
     Arguments:
         amount: int
             amount in mSatoshi
         currency: str
             BIP-173 currency code
+        unit: None or str
+            str:  Use the given unit
+            None: Use the default unit for the currency
     Returns: str
         the formatted amount
     Exceptions:
         none
     '''
-    #TODO: make the unit configurable
     #Maybe also pass the precision as argument?
 
-    digits = '%012d' % amount
-    BTC  = digits[:-11]
-    Sat  = digits[-11:-3]
-    mSat = digits[-3:]
+    info = currencyInfo[currency]
+    if unit is None:
+        unit = info.defaultUnit
+    multiplier = info.multipliers[unit]
 
-    return '%s.%s %s BTC' % (BTC, Sat, mSat)
+    sign = '-' if amount < 0 else ''
+    amount = abs(amount)
+
+    ret = ' ' + unit
+
+    if multiplier == 1:
+        #No decimal separator
+        return '%s%d%s' % (sign, amount, ret)
+    elif multiplier > 1000:
+        #Sub-satoshi part after a space, if we we use a more-than-satoshi unit
+        ret = ' %03d%s' % (amount % 1000, ret)
+        amount //= 1000
+        multiplier //= 1000
+
+    while multiplier >= 10:
+        ret = str(amount % 10) + ret
+        amount //= 10
+        multiplier //= 10
+
+    assert(multiplier == 1)
+
+    return '%s%d.%s' % (sign, amount, ret)
 
 
-def unformatAmount(text):
+def unformatAmount(text, currency='bc'): #TODO: assume no default!
     '''
     Arguments:
         text: str
@@ -83,11 +155,16 @@ def unformatAmount(text):
             [x][.y]U
             where x and y consist of decimal characters, and U is a recognized
             unit. Whitespace is ignored.
+        currency: str
+            BIP-173 currency code
     Returns: int
         amount in mSatoshi
     Exceptions:
         Exception: syntax error
     '''
+
+    info = currencyInfo[currency]
+
     #Remove whitespace:
     for c in ' \t\r\n':
         text = text.replace(c, '')
@@ -99,7 +176,7 @@ def unformatAmount(text):
     unit = text[lastDecimalPos+1:]
     text = text[:lastDecimalPos+1]
 
-    multiplier = amountUnits[unit]
+    multiplier = info.multipliers[unit]
     amount = multiplier * decimal.Decimal(text)
     return int(amount)
 
